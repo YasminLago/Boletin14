56e00f8507cc487428ab1a79479803df24d59117
YasminLago <ylagorebollar@danielcastelao.org>
2016-01-26 09:03:05 +0100

    Update ConversorTemperaturas.java

--- src/empresa/ConversorTemperaturas.java
+++ src/empresa/ConversorTemperaturas.java
@@ -1,24 +1,32 @@
 package empresa;
 //Yasmín
+
 public class ConversorTemperaturas {
-    final static float TEMPMINIMACENT=80;
-    
-    public float centigradosAFharenheit(float gC)throws TemperaturaErradaExcepcion{
+
+    final static float TEMPMINIMACENT = 79;
+
+    public float centigradosAFharenheit(float gC) throws TemperaturaErradaExcepcion {
         float gF;
-        if(gC<TEMPMINIMACENT)
-            throw new TemperaturaErradaExcepcion(" La temperatura en ºC debe ser mayor de 80");
-        else
+        if (gC < TEMPMINIMACENT) {
+            throw new TemperaturaErradaExcepcion(" La temperatura en ºC debe ser igual o mayor de 80");
+        } else {
             System.out.println("Temperatura en grados Fharenheit: ");
-        return gF=(float) (900/5.0*gC+32.4);
+        }
+        return gF = (float) (900 / 5.0 * gC + 32.4);
     }
-    public void centigradosAReaumur(float gC){ 
-        try{
-       System.out.println("Temperatura en grados Reaumur: "+(4.0/5.0*gC));
-    }catch(ArithmeticException ex){
-         System.out.println("Operación invalida: "+ex.toString());
-       }
-        finally{
-          System.out.println(" ***** a sentenza finally executase sempre");
-      }
-    }    
+
+    public void centigradosAReaumur(float gC)throws TemperaturaErradaExcepcion {
+        if (gC < TEMPMINIMACENT) 
+            try {      
+                throw new TemperaturaErradaExcepcion(" La temperatura en ºC debe ser igual o mayor de 80");
+            } catch (TemperaturaErradaExcepcion ex) {
+                System.out.println("Operación invalida: " + ex.toString());
+            } finally {
+                System.out.println(" ***** a sentenza finally executase sempre");
+            }
+        else{
+            System.out.println("Temperatura en grados Reaumur: " + (4.0 / 5.0 * gC));
+        }
+        
+    }
 }
